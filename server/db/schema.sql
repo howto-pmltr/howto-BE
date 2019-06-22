965bc0950e762cbb63d72400fefa8ef12d9ae0fd
Andy Bettisworth <andy@accreu.com>
2019-06-22 18:51:18 -0500

    Document schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,4 +1,10 @@
 BEGIN TRANSACTION;
 
+CREATE TABLE `users` (
+  `id` integer not null primary key autoincrement,
+  `email` text not null,
+  `created_at` datetime not null default CURRENT_TIMESTAMP,
+  `updated_at` datetime not null default CURRENT_TIMESTAMP
+);
 
 COMMIT TRANSACTION;
