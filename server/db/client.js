3f983088320f1333a81acda07f0fedef454ffb5a
Andy Bettisworth <andy@accreu.com>
2019-06-22 18:48:17 -0500

    Create knex db client

---
+++ server/db/client.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const knex = require('knex')
+const knexfile = require('../../knexfile')
+
+/**
+ * Define client
+ */
+
+let client
+
+switch(process.env.NODE_ENV) {
+  case 'production':
+    client = knex(knexfile.production)
+    break
+  case 'test':
+    client = knex(knexfile.test)
+    break
+  default:
+    client = knex(knexfile.development)
+}
+
+/**
+ * Export client
+ */
+
+module.exports = client
