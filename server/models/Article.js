7c638144015c33d6331de6e51ef05e7c135a73da
Andy Bettisworth <andy@accreu.com>
2019-06-22 21:51:47 -0500

    Add Article model

---
+++ server/models/Article.js
@@ -0,0 +1,52 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const db = require('../db/client')
+
+/**
+ * Define model
+ */
+
+class Article {
+  static async all() {
+    return await db('articles')
+  }
+
+  static async create(article) {
+    const [id] = await db('articles').insert({
+      title: article.title
+    })
+
+    const new_article = await db('articles').where({ id: id }).first()
+
+    return new_article
+  }
+
+  static async find(filter) {
+    return await db('articles').where(filter).first()
+  }
+
+  static async update(id, article) {
+    const changes = {}
+    changes.updated_at = new Date()
+
+    await db('articles').where({ id: id }).update(changes)
+
+    const new_article = await db('articles').where({ id: id }).first()
+
+    return new_article
+  }
+
+  static async destroy(id) {
+    return await db('articles').where({ id: id }).del()
+  }
+}
+
+/**
+ * Export model
+ */
+
+module.exports = Article
