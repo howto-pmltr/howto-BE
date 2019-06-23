33c907d5b3e21362d203de4adb8c3157873d8fa4
Andy Bettisworth <andy@accreu.com>
2019-06-23 00:13:38 -0500

    Add AuthController

---
+++ server/config/secrets.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+  jsonwebtoken_secret: process.env.JSONWEBTOKEN_SECRET || 'adb9944c77871d0c5fabf5084d846e52a5ab7a8b0bc65c3f1ef2af7e7a5efe539bbe55767c84919ab7204f9aaf577c1453ad91937392c3c9ed2946c48a318994'
+}
