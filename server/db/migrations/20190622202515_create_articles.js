8d36a633005660aff9894e728bca0268bb36993c
Andy Bettisworth <andy@accreu.com>
2019-06-22 20:45:53 -0500

    Run migrations

--- server/db/migrations/20190622202515_create_articles.js
+++ server/db/migrations/20190622202515_create_articles.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('articles', table => {
     table.increments('id')
     table.text('author_email').notNullable()
-    table.text('title').notnullable()
+    table.text('title').notNullable()
     table.text('description')
     table.datetime('published_at')
     table.timestamps(true, true)
