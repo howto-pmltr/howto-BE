b3c4f8f4c32b684b98d53f2654d1bc7f5ef8f418
Andy Bettisworth <andy@accreu.com>
2019-06-24 13:55:10 -0500

    Update users seeded data

--- server/db/seeds/001-users.js
+++ server/db/seeds/001-users.js
@@ -4,8 +4,8 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {id: 1, email: 'john@company.com'},
-        {id: 2, email: 'jane@company.com'},
+        {id: 1, username: 'john', email: 'john@company.com', password_hash: '$2a$10$dFIAtS9lM3K2zNhbOnx3tuCG04MpXRd/lMMZ64PHe3G/8KNZMPHSK'},
+        {id: 2, username: 'jane', email: 'jane@company.com', password_hash: '$2a$10$dFIAtS9lM3K2zNhbOnx3tuCG04MpXRd/lMMZ64PHe3G/8KNZMPHSK'},
       ])
     })
 }
