a1dbfe72673c9b4943bfcf9891a8667f86d9958f
Andy Bettisworth <andy@accreu.com>
2019-06-27 17:31:36 -0500

    Test GET /tags - success

--- server/test/routes/tags_router.test.js
+++ server/test/routes/tags_router.test.js
@@ -34,10 +34,46 @@ describe('routes', () => {
   })
 
   describe('tags_router.js', () => {
-    test.todo('GET /tags - success')
-    test.todo('GET /tags - return empty array if no tags')
-    test.todo('POST /tags - success')
-    test.todo('POST /tags - missing request body')
-    test.todo('POST /tags - missing request body fields')
+    test.only('GET /tags - success', async () => {
+      const res = await supertest(app).get('/tags')
+      expect(res.status).toBe(200)
+      expect(res.type).toBe('application/json')
+      expect(res.body).toBeTruthy()
+      expect(res.body.constructor).toBe(Array)
+      expect(res.body.length).toBe(3)
+    })
+
+    test('GET /tags - return empty array if no tags', async () => {
+      await db('tags').del()
+
+      const res = await supertest(app).get('/tags')
+      expect(res.status).toBe(200)
+      expect(res.type).toBe('application/json')
+      expect(res.body).toBeTruthy()
+      expect(res.body.constructor).toBe(Array)
+      expect(res.body.length).toBe(0)
+    })
+
+    test('POST /tags - success', async () => {
+      const res = await supertest(app).post('/tags')
+      expect(res.status).toBe(200)
+      expect(res.type).toBe('application/json')
+      expect(res.body).toBeTruthy()
+    })
+
+    test('POST /tags - missing request body', async () => {
+      const res = await supertest(app).post('/tags')
+      expect(res.status).toBe(200)
+      expect(res.type).toBe('application/json')
+      expect(res.body).toBeTruthy()
+    })
+
+    test('POST /tags - missing request body fields', async () => {
+      const res = await supertest(app).post('/tags')
+      expect(res.status).toBe(200)
+      expect(res.type).toBe('application/json')
+      expect(res.body).toBeTruthy()
+    })
+
   })
 })
